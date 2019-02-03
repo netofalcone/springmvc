c0f7d757b8533a6a98397144719a0f64af45c1f7
Neto Falcone <netofalcone@gmail.com>
2019-02-03 15:51:18 -0200

    first commit

---
+++ src/main/java/curso/springboot/repository/PessoaRepository.java
@@ -0,0 +1,21 @@
+package curso.springboot.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import curso.springboot.model.Pessoa;
+
+
+@Repository
+@Transactional
+public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
+	
+	@Query("select nome from Pessoa where nome like %?1%")
+	 List<Pessoa> findPessoaByName(String nome);
+
+
+}
